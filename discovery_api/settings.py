50a2821a2ab26dffa8424551e5d3cde3b4c1be01
klim <klim.vlad@list.ru>
2025-10-22 15:44:45 +0300

    Add image previews in admin

--- discovery_api/settings.py
+++ discovery_api/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'imagekit',  
     'places',
+    'adminsortable2',
 ]
 
 MIDDLEWARE = [
--- places/admin.py
+++ places/admin.py
@@ -4,18 +4,17 @@ from .models import Place, Image
 
 class ImageInline(admin.TabularInline):
     model = Image
-    extra = 3
-    readonly_fields = ['image_preview']
-
+    extra = 1
+    fields = ['image_preview', 'image', 'position']  
+    readonly_fields = ['image_preview'] 
     def image_preview(self, obj):
         if obj.image:
-            return format_html('<img src="{}" style="max-height: 100px; max-width: 100px;" />', obj.image.url)
-        return "No image"
+            return format_html('<img src="{}" style="max-height: 200px; max-width: 200px;" />', obj.image.url)
+        return "No image uploaded"
 
 @admin.register(Place)
 class PlaceAdmin(admin.ModelAdmin):
     inlines = [ImageInline]
-    list_display = ['title', 'lng', 'lat']
 
 @admin.register(Image)
 class ImageAdmin(admin.ModelAdmin):
@@ -24,5 +23,5 @@ class ImageAdmin(admin.ModelAdmin):
 
     def image_preview(self, obj):
         if obj.image:
-            return format_html('<img src="{}" style="max-height: 50px; max-width: 50px;" />', obj.image.url)
+            return format_html('<img src="{}" style="max-height: 100px; max-width: 100px;" />', obj.image.url)
         return "No image"
